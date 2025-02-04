e27d62b5495a2c8f197005c53aaf9b3ad8777445
Jorme8 <a27905@svalero.com>
2025-02-04 12:59:15 +0100

    Modify build.gradle for dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,15 @@ android {
 
 dependencies {
 
+    implementation ("androidx.preference:preference:1.2.0")
+    implementation("com.mapbox.maps:android:10.9.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.4.0")
+    implementation("androidx.annotation:annotation:1.6.0")
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
