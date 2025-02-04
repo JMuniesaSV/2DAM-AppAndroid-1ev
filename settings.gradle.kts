ae52cef3e6ed8cb6d8a952004a786d6db17e868d
Jorme8 <a27905@svalero.com>
2025-02-04 14:42:56 +0100

    a

---
+++ settings.gradle.kts
@@ -0,0 +1,38 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = "sk.eyJ1IjoiYTI3NzY4IiwiYSI6ImNtM3Fmdnp0bTBoOXAybXNhaGlyYjM2am8ifQ.ay66rlMiQpl51ofHH7Zedw"
+            }
+        }
+    }
+}
+
+rootProject.name = "Transportes"
+include(":app")
+ 
